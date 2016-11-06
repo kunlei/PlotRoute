66a225189876165d97f157e6e16f5877f05a97a8
Kunlei Lian <Kunlei Lian>
2016-11-06 00:04:30 -0500

    add data processing

--- main.R
+++ main.R
@@ -4,3 +4,17 @@ library(ggplot2)
 
 source("Multi_Plot_Func.R")
 
+con <- file("best-n10-d3-c3.txt", "r")
+data <- readLines(con)
+close(con)
+
+NodeLoc <- data.table(id = 0 : 10,
+                      xcoord = as.numeric(unlist(strsplit(data[1], " "))),
+                      ycoord = as.numeric(unlist(strsplit(data[2], " "))))
+setkey(NodeLoc, "id")
+
+Routes <- list(d1 = as.integer(unlist(strsplit(data[3], " "))),
+                     d2 = as.integer(unlist(strsplit(data[4], " "))),
+                     d3 = as.integer(unlist(strsplit(data[5], " "))))
+
+PlotRoute(NodeLoc, Routes)
